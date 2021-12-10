f2cae05f1886935761380aa3b1096da4bd8ccf03
torippy917 <b1900228@planet.kanazawa-it.ac.jp>
2021-12-10 11:21:08 +0900

    feat: add write(), delete open/close_gripper()

--- open_manipulator_controller/scripts/write_char.py
+++ open_manipulator_controller/scripts/write_char.py
@@ -25,11 +25,16 @@ rv2q = rotationVector2quaternion
 
 
 class Arm:
-    def __init__(self):
+    def __init__(self, resolution = 0.05, log = True):
         self.group = MoveGroupCommander("arm")
         self.gripper_group = MoveGroupCommander("gripper")
         self.group.set_planning_time(600.0)
         self.gripper_joint_angle = self.gripper_group.get_current_joint_values()
+        self.resolution = resolution
+        self.z_write = 0.08
+        self.z_move = 0.1
+        self.pitch_write = 0.0
+        self.log = log
 
     def moveGripper(self, angle):
         self.gripper_joint_angle[0] = angle
@@ -59,9 +64,19 @@ class Arm:
         pose.orientation.z = quat.z
         self.group.set_joint_value_target(pose, True)
         self.group.go()
+        if self.log:
+            self.logPose()
 
     def moveDefault(self):
         self.move(0.2, 0.0, 0.25, 0.0)
+
+    def write(self, line):
+        self.move(line[0][0], line[0][1], self.z_move, self.pitch_write)
+        for i in range(len(line)-1):
+            n = math.ceil(math.hypot(line[i+1][0]-line[i][0], line[i+1][1]-line[i][1])/self.resolution)
+            for t in [i/n for i in range(n+1)]:
+                self.move(t*line[i+1][0]+(1-t)*line[i][0], t*line[i+1][1]+(1-t)*line[i][1], self.z_write, self.pitch_write)
+        self.move(line[-1][0], line[-1][1], self.z_move, self.pitch_write)
 
     def logPose(self):
         pose_current = self.group.get_current_pose()
@@ -74,106 +89,15 @@ def main():
 
     arm = Arm()
     arm.moveDefault()
-    arm.logPose()
     arm.holdBrush()
-    arm.logPose()
 
+    line0 = [[0.22, 0.2],[0.22, -0.2]]
+    line1 = [[0.35,0],[0.2,0]]
+    arm.write(line0)
+    arm.write(line1)
 
-# グリッパを開く 
-def open_gripper():
-    print("Opening Gripper...")
-
-    # gripper_joint_angle[0] = -0.01 # 実機：-0.01, シミュレータ： 0.01
-    gripper_joint_angle[0] = 0.01 # 実機：-0.01, シミュレータ： 0.01
-
-    gripper_group.set_joint_value_target(gripper_joint_angle)
-    plan2 = gripper_group.go()
-    gripper_group.stop()
-    gripper_group.clear_pose_targets()
-    # rospy.sleep(1)
-
-
-# グリッパを閉じる
-def close_gripper():
-
-    print("Closing Gripper...")
-    # gripper_joint_angle[0] = 0.0 # 実機：0.0，シミュレータ：-0.01
-    gripper_joint_angle[0] = -0.01 # 実機：0.0，シミュレータ：-0.01
-    gripper_group.set_joint_value_target(gripper_joint_angle)
-    plan2 = gripper_group.go()
-    gripper_group.stop()
-    gripper_group.clear_pose_targets()
-    # rospy.sleep(1)
+    arm.moveDefault()
 
 
 if __name__ == '__main__':
-    
-    node_name = "arm_demo2"
-    rospy.init_node(node_name, anonymous=True ) # ノードの初期化
-
     main()
-
-    exit()
-
-    ## MoveGroupCommanderオブジェクトのインスタンス生成。これがジョイントへのインタフェースになる。
-    ## このインタフェースは運動計画と動作実行に使われる。
-    group = MoveGroupCommander("arm")
-    gripper_group = MoveGroupCommander("gripper") # 追加
-    
-    group.set_planning_time(600.0) # 動作計画に使う時間[s]の設定
-    
-    # 初期姿勢の取得
-    pose_init = group.get_current_pose()  # エンドエフェクタの位置(x, y, z)を取得
-    rospy.loginfo( "Get Initial Pose\n{}".format( pose_init ) )
-    rpy_init  = group.get_current_rpy()   # エンドエフェクタの姿勢(roll, pitch, yaw)を取得
-    rospy.loginfo( "Get Initial RPY:{}".format( rpy_init ) )
-    
-    # グリッパの初期値を取得
-    gripper_joint_angle = gripper_group.get_current_joint_values()
-    print("Get Current Gripper angle:\n{}\n".format(gripper_joint_angle))
-
-
-    # 姿勢の定義
-    # 初期姿勢
-    rospy.loginfo( "Starting Pose Init")    
-    pose_init = Pose()
-    pose_init.position.x =  0.2
-    pose_init.position.y =  0.0
-    pose_init.position.z =  0.1
-    theta = math.pi/2
-    v = [0, 1, 0]
-    pose_init.orientation.x =  v[0]*math.sin(theta/2)  # 姿勢(クオータニオン)
-    pose_init.orientation.y =  v[1]*math.sin(theta/2)  # 姿勢(クオータニオン)
-    pose_init.orientation.z =  v[2]*math.sin(theta/2)  # 姿勢(クオータニオン)
-    pose_init.orientation.w =  math.cos(theta/2) # 姿勢(クオータニオン)  
-    
-    # 姿勢1
-    rospy.loginfo( "Starting Pose 1")    
-    pose_target_1 = Pose()
-    pose_target_1.position.x =  0.15
-    pose_target_1.position.y =  0.10
-    pose_target_1.position.z =  0.048
-    pose_target_1.orientation.x = -0.2706  # 姿勢(クオータニオン)
-    pose_target_1.orientation.y =  0.6533  # 姿勢(クオータニオン)
-    pose_target_1.orientation.z =  0.2706  # 姿勢(クオータニオン)
-    pose_target_1.orientation.w =  0.6533  # 姿勢(クオータニオン)  
-
-
-    # 動作開始
-    group.set_joint_value_target(pose_init, True)
-    group.go()
-    rospy.sleep(5.0)
-    pose_current = group.get_current_pose()
-    rospy.loginfo( "Get Current Pose:\n{}\n".format( pose_current ) )
-
-    open_gripper()
-    rospy.sleep(2.0)
-    close_gripper()
-    
-    # 初期姿勢に戻る
-    rospy.loginfo( "Back to Initial Pose")
-    group.set_joint_value_target(pose_init, True)
-    group.go()
-    rospy.sleep(5.0)
-    pose_current = group.get_current_pose()
-    rospy.loginfo( "Get Current Pose:\n{}\n".format( pose_current ) )
